680ea20eec3edd63984f71710ea1bd7fc422dc36
Sneha Deshmukh <sneha.biocode@gmail.com>
2025-12-08 19:50:57 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,4 @@
+name = input("Enter your first name: ")
+sir_name = input("Enter your last name: ")
+full_name = nam +" "+ sir_nam
+print("Hello, " + full_name + " ! " + "Welcome to the Python program.")
