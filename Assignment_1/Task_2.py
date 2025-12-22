c32d15b4abc2b1932f46f3dbe680e8e017701bc7
Sneha Deshmukh <sneha.biocode@gmail.com>
2025-12-22 11:53:10 +0530

    Delete Assignment_1 directory

--- Assignment_1/Task_2.py
+++
@@ -1,4 +0,0 @@
-nam = input("Enter your first name: ")
-sir_nam = input("Enter your last name: ")
-full_name = nam +" "+ sir_nam
-print("Hello, " + full_name + " ! " + "Welcome to the Python program.")
